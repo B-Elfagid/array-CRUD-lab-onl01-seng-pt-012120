16db345d06529475484f39022dbcf6c3997d868d
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-02-01 12:14:08 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,31 +1,10 @@
-def create_an_empty_array []
-  
+def create_an_array
+create_an_array["b", "c", "d", "e"]
+empty_array = []
+create_an_array << "f"
+create_an_array.unshift("g")
+create_an_array.pop
+create_an_array.shift 
+create_an_array[0]
 end
 
-def create_an_array("betty", "patty", "dave", "mel")
-  create_an_array << "marthi"
-  
-  
-def add_element_to_start_of_array(array, element)
-  
-end
-
-def remove_element_from_end_of_array(array)
-  
-end
-
-def remove_element_from_start_of_array(array)
-  
-end
-
-def retrieve_element_from_index(array, index_number)
-  
-end
-
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
-  
-end
