8b8175a80ba4486f1fc7d77c2db334473e8a54a5
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-02-01 15:27:50 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,8 +1,38 @@
-create_an_array["b", "c", "d", "e"]
-create_an_array << "f"
-create_an_array.unshift("g")
-create_an_array.pop
-create_an_array.shift 
-create_an_array[0]
+def create_an_empty_array
+  []
 end
+
+def create_an_array
+  ["betty", "dave", "mel", "pat"]
+end
+
+def add_element_to_end_of_array(array, element)
+  array << element
+end
+
+def add_element_to_start_of_array(array, element)
+  array.unshift(element)
+end
+
+def remove_element_from_end_of_array(array)
+  array.pop
+end
+
+def remove_element_from_start_of_array(array)
+  array.shift
+end
+
+def retreive_element_from_index(array, index_number)
+   array[index_number]
+ end
+ 
+ def retreive_first_element_from_array(array)
+   array[0]
+ end
+ 
+ def retreive_last_element_from_array(array)
+    x = array.count
+    array[x-1]
+  end
+  
 
