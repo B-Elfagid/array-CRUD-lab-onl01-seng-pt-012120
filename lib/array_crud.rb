d07961a201c20659332a92296830fe326c4def4f
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-02-01 11:58:46 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,15 +1,11 @@
-def create_an_empty_array
+def create_an_empty_array []
   
 end
 
-def create_an_array
+def create_an_array("betty", "patty", "dave", "mel")
+  create_an_array << "marthi"
   
-end
-
-def add_element_to_end_of_array(array, element)
   
-end
-
 def add_element_to_start_of_array(array, element)
   
 end
